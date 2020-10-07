2ce9271159251ba3e5bffe4ae386dcc0731b68f8
abel123-max <67727569+abel123-max@users.noreply.github.com>
2020-10-07 13:57:49 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -3,6 +3,10 @@ var signe=""
 var signeO=""
 var random=""
 var nombre = 0;
+var score_u = 0;
+var score_o = 0;
+var led_j ="";
+var led_o ="";
 function change() {
     var person = prompt("Please enter your name",);
 
@@ -12,99 +16,99 @@ function change() {
 }
 function changement_de_jeux1(signe) {
     if (signe == "pierre") {
-        document.getElementById("mdimg").src = "pierre.svg"
-        signeU = "pierre"
+        document.getElementById("mdimg").src = "pierre.svg";
+        signeU = "pierre";
+        console.log("pierre");
     }
 
     else if (signe == "feuille") {
 
-        document.getElementById("mdimg").src = "feuille.svg"
-        signeU = "feuille"
+        document.getElementById("mdimg").src = "feuille.svg";
+        signeU = "feuille";
+        console.log("feuille");
     }
     else if (signe == "ciseaux") {
 
-        document.getElementById("mdimg").src = "ciseaux.svg"
-        signeU = "ciseaux"
-
+        document.getElementById("mdimg").src = "ciseaux.svg";
+        signeU = "ciseaux";
+        console.log("ciseaux");
     }
-    utilisateur_win();
+    changement_de_jeux2();
+    nombre++
+    console.log("nombre:" + nombre);
+    document.getElementById("nombre").innerText = nombre;
+    win_j();
 }
 function changement_de_jeux2() {
 var random = Math.round(Math.random() * 3)
 if (random == 1) {
-    document.getElementById("mdimg2").src = "pierre.svg"
-    signeO ="pierre" 
+    document.getElementById("mdimg2").src = "pierre.svg";
+    signeO ="pierre"; 
+    console.log("pierre");
+    
 }
 else if (random == 2) {
-    document.getElementById("mdimg2").src = "feuille.svg"
-    signeO = "feuille"
-
-}
-else if (random == 3) {
-    document.getElementById("mdimg2").src = "feuille.svg"
-    signeO = "ciseaux"
-
-
-}
-utilisateur_win();
-}
-function utilisateur_win() {
-    if (signeU == signeO) {
-        console.log("egality" + " " + signeU + " " + signeO);
-        if (nombre == 1) {
-            console.log("egality");
-            document.getElementById("ledo1").style.backgroundColor = 'blue';
-            document.getElementById("led1").style.backgroundColor = 'blue';
-        }
-        else if (nombre == 2) {
-            console.log("egality");
-            document.getElementById("ledo2").style.backgroundColor = 'blue';
-            document.getElementById("led2").style.backgroundColor = 'blue';
-        }
-        else {
-            console.log("egality");
-            document.getElementById("ledo3").style.backgroundColor = 'blue';
-            document.getElementById("led3").style.backgroundColor = 'blue';
-        }
-    }
-}
- if (signeU == "pierre" && signeO == "ciseaux" || signeU == "feuille" && signeO == "pierre" || signeU == "ciseaux" && signeO == "feuille") {
-    console.log("win" + " " + signeU + " " + signeO);
-    if (nombre == 1) {
-        console.log("win");
-        document.getElementById("ledo1").style.backgroundColor = 'red';
-        document.getElementById("led1").style.backgroundColor = 'green';
-    }
-
-    else if (nombre == 2) {
-        console.log("win");
-        document.getElementById("ledo2").style.backgroundColor = 'red';
-        document.getElementById("led2").style.backgroundColor = 'green';
-    }
-else  {
-        console.log("win");
-        document.getElementById("ledo3").style.backgroundColor = 'red';
-        document.getElementById("led3").style.backgroundColor = 'green';
-    }
-}
-else {
-console.log("lose" + " " + signeU + " " + signeO);
-if (nombre == 1) {
-    console.log("win");
-    document.getElementById("ledo1").style.backgroundColor = 'green';
-    document.getElementById("led1").style.backgroundColor = 'red';
-}
-
-else if (nombre == 2) {
-    console.log("win");
-    document.getElementById("ledo2").style.backgroundColor = 'green';
-    document.getElementById("led2").style.backgroundColor = 'red';
+    document.getElementById("mdimg2").src = "feuille.svg";
+    signeO = "feuille";
+    console.log("feuille");
 }
 else  {
-    console.log("win");
-    document.getElementById("ledo3").style.backgroundColor = 'green';
-    document.getElementById("led3").style.backgroundColor = 'red';
+    document.getElementById("mdimg2").src = "ciseaux.svg";
+    signeO = "ciseaux";
+    console.log("ciseaux");
 }
-
-
+}
+function win_j() {
+    switch (nombre) {
+        case 1:
+            if (nombre == 1) {
+                led_j = 'led1';
+                led_o = 'ledo1';
+                break;
+            }
+        case 2:
+            if (nombre == 2) {
+                led_j = 'led2';
+                led_o = 'ledo2';
+                break;
+            }
+        case 3:
+            if (nombre == 3) {
+                led_j = 'led3';
+                led_o = 'ledo3';
+                break;
+            }
+    }
+    if (signeU==signeO) {
+        alert ("egalité");
+        document.getElementById(led_j).style.backgroundColor = 'blue';
+        document.getElementById(led_o).style.backgroundColor = 'blue';
+    }
+    else if (signeU == 'pierre' && signeO == 'ciseaux' || signeU == 'feuille' && signeO == 'pierre' || signeU == 'ciseaux' && signeO == 'feuille') {
+        alert ("gagner");
+        document.getElementById(led_j).style.backgroundColor = 'green';
+        document.getElementById(led_o).style.backgroundColor = 'red';
+        score_u++;
+    }
+    else {
+        alert ("perdu");
+        document.getElementById(led_j).style.backgroundColor = 'red';
+        document.getElementById(led_o).style.backgroundColor = 'green';
+        score_o++;
+    }
+while (nombre >= 3) {
+    if (score_u>score_o) {
+        console.log("trankilouse ou la gagner");
+        document.getElementById("nombre").innerHTML= score_u + "-" + score_o + " " + "gagner"
+    }
+    else if (score_u<score_o) {
+        console.log("trankilouse ou la perd");
+        document.getElementById("nombre").innerHTML= score_u + "-" + score_o + " " + "perdu"
+    }
+    else {
+        console.log("égalité");
+        document.getElementById("nombre").innerHTML= score_u + "-" + score_o + " " + "egalité"
+    }
+    break;
+}
 }
